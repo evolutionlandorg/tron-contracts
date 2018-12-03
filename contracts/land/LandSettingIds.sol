b5fbfc3785a26e25e5c9803842b7023e77cefb7f
HackFisher <hackfisher@gmail.com>
2018-12-03 17:09:19 +0800

    add package json

--- contracts/land/LandSettingIds.sol
+++ contracts/land/LandSettingIds.sol
@@ -5,12 +5,4 @@ import "../common/interfaces/ISettingsRegistry.sol";
 
 contract LandSettingIds is SettingIds {
 
-    uint256 public constant GOLD_MAGNITUDE = 1;
-    uint256 public constant WOOD_MAGNITUDE = 1;
-    uint256 public constant WATER_MAGNITUDE = 1;
-    uint256 public constant FIRE_MAGNITUDE = 1;
-    uint256 public constant SOIL_MAGNITUDE = 1;
-
-    bytes32 public constant CONTRACT_MINER = "CONTRACT_MINER";
-
 }
