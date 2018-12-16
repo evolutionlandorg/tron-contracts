0a9290908a34ba7fcf1cc16993f920a04219fb61
HackFisher <hackfisher@gmail.com>
2018-12-16 16:37:32 +0800

    remove comment

--- migrations/11_deploy_shasta.js
+++ migrations/11_deploy_shasta.js
@@ -87,7 +87,7 @@ module.exports = function(deployer, network, accounts) {
     if (network == "shasta")
     {
         deployer.then(async () => {
-            // await shastaDeploy(deployer, network, accounts);
+            await shastaDeploy(deployer, network, accounts);
         });
     }
 };
