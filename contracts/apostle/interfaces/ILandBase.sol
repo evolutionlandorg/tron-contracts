6febaf0aff65296442cfc9d016e4ad14a1f36434
HackFisher <hackfisher@gmail.com>
2019-01-03 14:02:06 +0800

    Fix LandBase

--- contracts/apostle/interfaces/ILandBase.sol
+++ contracts/apostle/interfaces/ILandBase.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.4.23;
 contract ILandBase {
 
     function resourceToken2RateAttrId(address _resourceToken) public view returns (uint256);
-    //function getResourceRate(uint _landTokenId, address _resouceToken) public view returns (uint16);
 }
